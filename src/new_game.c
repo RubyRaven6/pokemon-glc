75ae93820b7770645be2c869d2b5f5a7babb425e
RubyRaven6 <swampertspawner@gmail.com>
2026-06-01 15:46:18 +0800

    new starting map

--- src/new_game.c
+++ src/new_game.c
@@ -140,7 +140,7 @@ static void WarpToTruck(void)
     if (IS_FRLG)
         SetWarpDestination(MAP_GROUP(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), MAP_NUM(MAP_PALLET_TOWN_PLAYERS_HOUSE_2F), WARP_ID_NONE, 6, 6);
     else
-        SetWarpDestination(MAP_GROUP(MAP_TESTING_GROUNDS), MAP_NUM(MAP_TESTING_GROUNDS), WARP_ID_NONE, -1, -1);
+        SetWarpDestination(MAP_GROUP(MAP_TESTING_GROUNDS), MAP_NUM(MAP_TESTING_GROUNDS), WARP_ID_NONE, 4, 5);
     WarpIntoMap();
 }
 
