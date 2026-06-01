bd7bf68abaab812dc066273de73dc5c344082597
RubyRaven6 <swampertspawner@gmail.com>
2026-06-01 20:32:40 +0800

    progress on tournament logic

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1754,5 +1754,4 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/TestingGrounds/scripts.scr"
 	
 	.include "data/maps/TestingGrounds_HouseFoyer/scripts.scr"
-
-	.include "data/maps/TestingGrounds_BattleArena/scripts.inc"
+	.include "data/maps/TestingGrounds_BattleArena/scripts.scr"
