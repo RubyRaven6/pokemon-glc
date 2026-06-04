27f13ccf2739c2643d3e0bf8f79c562552a4d78d
RubyRaven6 <swampertspawner@gmail.com>
2026-06-04 21:05:14 +0800

    fixed assert

--- src/battle_message.c
+++ src/battle_message.c
@@ -2936,7 +2936,7 @@ static const u8 *BattleStringGetOpponentNameByTrainerId(u16 trainerId, u8 *text,
             toCpy = GetTrainerNameFromId(trainerId);
     }
 
-    assertf(DoesStringProperlyTerminate(toCpy, TRAINER_NAME_LENGTH + 1),"Opponent needs a valid name")
+    assertf(toCpy != NULL, "Opponent needs a valid name");
     {
         return sText_EmptyString4;
     }
