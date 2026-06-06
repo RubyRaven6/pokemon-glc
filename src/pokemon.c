d5e1c41cf9c4b8d6e9559f8b2494491d9e8429d7
RubyRaven6 <swampertspawner@gmail.com>
2026-06-07 00:08:51 +0800

    progress of making new movesets

--- src/pokemon.c
+++ src/pokemon.c
@@ -457,6 +457,7 @@ const struct NatureInfo gNaturesInfo[NUM_NATURES] =
 #include "data/pokemon/experience_tables.h"
 
 #include "data/pokemon/level_up_learnsets/gen_pglc.h"
+#include "data/pokemon/level_up_learnsets/gen_9.h"
 
 // #if P_LVL_UP_LEARNSETS >= GEN_9
 // #include "data/pokemon/level_up_learnsets/gen_9.h" // Scarlet/Violet
