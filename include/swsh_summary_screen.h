d0d3deb0211a57ccb1a3822436ae66af1e9b0383
RubyRaven6 <swampertspawner@gmail.com>
2026-06-06 12:59:37 +0800

    changed configs

--- include/swsh_summary_screen.h
+++ include/swsh_summary_screen.h
@@ -16,7 +16,7 @@
 #define SWSH_SUMMARY_NATURE_COLORS                    TRUE      // color stats increased or reduced by nature, red = boosted, blue = reduced
 #define SWSH_SUMMARY_CATEGORY_ICONS                   TRUE      // determines whether category (split) icons are shown or not
 #define SWSH_SUMMARY_SHOW_IV_EV                       TRUE      // determines how to show IVs and EVs
-#define SWSH_SUMMARY_SHOW_FRIENDSHIP                  TRUE      // show a heart that fills up to indicate friendship value
+#define SWSH_SUMMARY_SHOW_FRIENDSHIP                  FALSE      // show a heart that fills up to indicate friendship value
 #define SWSH_SUMMARY_SWSH_STATUS_ICONS                TRUE      // use Gen 8 style status icons instead of default ones.
 #define SWSH_SUMMARY_SWSH_TYPE_ICONS                  TRUE      // use Gen 8 style type icons instead of the default ones
 #define SWSH_SUMMARY_SWSH_TYPE_ICONS_SV_PAL           FALSE     // use Scarlet/Violet palette for type icons
@@ -25,10 +25,10 @@
 #define SWSH_SUMMARY_BG_BLEND                         FALSE     // enables alpha blending for the main UI (semi-transparency)
 #define SWSH_SUMMARY_MON_IDLE_ANIMS                   TRUE      // loops the mon animations regularly as an "idle" anim
 #define SWSH_SUMMARY_MON_SHADOWS                      TRUE      // displays a shadow for the mon sprite
-#define SWSH_SUMMARY_SHOW_CONTEST_PAGES               TRUE      // enables conditions and contest moves pages
+#define SWSH_SUMMARY_SHOW_CONTEST_PAGES               FALSE      // enables conditions and contest moves pages
 #define SWSH_SUMMARY_SHOW_DYNAMAX_LEVEL               FALSE     // show dynamax level
 #define SWSH_SUMMARY_SHOW_GIGANTAMAX                  FALSE     // show gigantamax icon
-#define SWSH_SUMMARY_SHOW_TERA_TYPE                   FALSE     // show tera type icons
+#define SWSH_SUMMARY_SHOW_TERA_TYPE                   TRUE     // show tera type icons
 
 // Looking for configs for renaming mons and relearning moves? Those use the standard expansion configs
 // P_SUMMARY_SCREEN_RENAME and P_SUMMARY_SCREEN_MOVE_RELEARNER in include/config/pokemon.h
