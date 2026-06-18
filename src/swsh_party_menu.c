f20e741827ca098dff3b1fef27023d4cfafe38c7
RubyRaven6 <swampertspawner@gmail.com>
2026-06-18 10:30:11 +0800

    removed SetGpuReg block

--- src/swsh_party_menu.c
+++ src/swsh_party_menu.c
@@ -1354,21 +1354,7 @@ static void FreePartyPointers(void)
     DestroyMonSpritesGfxManager(MON_SPR_GFX_MANAGER_A);
     DestroyMoveTypeSprites();
     // Clear alpha blending from party mon shadows
-    SetGpuReg(REG_OFFSET_BLDCNT, 0);
-    SetGpuReg(REG_OFFSET_BLDALPHA, 0);
-    // SetGpuReg(REG_OFFSET_DISPCNT, 0);
     SetGpuReg(REG_OFFSET_DISPCNT, DISPCNT_OBJ_ON | DISPCNT_OBJ_1D_MAP);
-    SetGpuReg(REG_OFFSET_BG3CNT, 0);
-    SetGpuReg(REG_OFFSET_BG2CNT, 0);
-    SetGpuReg(REG_OFFSET_BG1CNT, 0);
-    SetGpuReg(REG_OFFSET_BG0CNT, 0);
-    SetGpuReg(REG_OFFSET_BLDY, 0);
-    SetGpuReg(REG_OFFSET_WIN0H, 0);
-    SetGpuReg(REG_OFFSET_WIN0V, 0);
-    SetGpuReg(REG_OFFSET_WIN1H, 0);
-    SetGpuReg(REG_OFFSET_WIN1V, 0);
-    SetGpuReg(REG_OFFSET_WININ, 0);
-    SetGpuReg(REG_OFFSET_WINOUT, 0);
 
     if (sPartyMenuInternal)
     {
